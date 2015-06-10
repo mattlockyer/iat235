01adeacbccad3432c8fb266df4b916deccccf93e
Matt Lockyer <mattdlockyer@gmail.com>
2015-06-10 07:45:11 -0700

    lab 4

--- js/app.js
+++ js/app.js
@@ -97,6 +97,10 @@ APP.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log)
                 {
                     name:'Lab 03',
                     fn:'lab-3.html'
+                },
+                {
+                    name:'Lab 04',
+                    fn:'lab-4.html'
                 }
             ]
         },
